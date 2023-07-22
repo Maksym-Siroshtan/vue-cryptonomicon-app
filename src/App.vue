ee40f3aee47336dba9b3c7d2a11cfc25eef052d0
Maksym <siroshtan.max1991ua@gmail.com>
2023-07-22 11:46:22 +0300

    Added saving filtered tickers to window state

--- src/App.vue
+++ src/App.vue
@@ -44,7 +44,7 @@
                 placeholder="Например DOGE"
               />
             </div>
-            <div class="flex bg-white shadow-md p-1 rounded-md flex-wrap">
+            <div class="flex bg-white shadow-md rounded-md flex-wrap">
               <span
                 v-for="coin in coinsFilter()"
                 @click="add(coin)"
@@ -83,9 +83,27 @@
 
       <template v-if="tickers.length">
         <hr class="w-full border-t border-gray-600 my-4" />
+        <div>
+          <button
+            v-if="page > 1"
+            @click="page = page - 1"
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            Назад
+          </button>
+          <button
+            v-if="hasNextPage"
+            @click="page = page + 1"
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            Вперёд
+          </button>
+          <div>Фильтр: <input v-model="filter" /></div>
+        </div>
+        <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="tiker in tickers"
+            v-for="tiker in filteredTickers()"
             :key="tiker.name"
             @click="select(tiker)"
             :class="{
@@ -180,15 +198,44 @@ export default {
       sel: null,
       added: false,
       graph: [],
+      page: 1,
+      filter: "",
+      hasNextPage: true,
     };
   },
 
   created() {
     this.getCoinsList();
     this.getDataByLocalStorage();
+
+    const windowData = Object.fromEntries(
+      new URL(window.location).searchParams.entries()
+    );
+
+    if (windowData.filter) {
+      this.filter = windowData.filter;
+    }
+
+    if (windowData.page) {
+      this.page = windowData.page;
+    }
   },
 
   methods: {
+    filteredTickers() {
+      // (this.page - 1) * 6, this.page * 6
+      const start = (this.page - 1) * 6;
+      const end = this.page * 6;
+
+      const filteredTickers = this.tickers.filter((ticker) =>
+        ticker.name.toLowerCase().includes(this.filter)
+      );
+
+      this.hasNextPage = filteredTickers.length > end;
+
+      return filteredTickers.slice(start, end);
+    },
+
     subscribeToUpdates(tickerName) {
       setInterval(async () => {
         const f = await fetch(
@@ -244,7 +291,7 @@ export default {
 
       if (!this.added) {
         this.tickers.push(currentTicker);
-
+        this.filter = "";
         localStorage.setItem(
           "cryptonomicon-list",
           JSON.stringify(this.tickers)
@@ -284,6 +331,24 @@ export default {
             : t.toLowerCase().indexOf(this.ticker.toLowerCase()) > -1
         )
         .slice(0, 4);
+    },
+  },
+  watch: {
+    filter() {
+      this.page = 1;
+
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      );
+    },
+    page() {
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      );
     },
   },
 };
